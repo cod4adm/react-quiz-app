f4ff0bf12b0adaaeca2c5982a18272b471fd96c8
cod4adm <cod4adm@gmail.com>
2020-04-26 17:50:59 +0300

    complete form for QuizCreator add axios connect Auth to firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from "is_js";
+import axios from "axios";
 
 
 export default class Auth extends Component {
@@ -36,11 +37,31 @@ export default class Auth extends Component {
         }
     };
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC__s0jesHKy_J07LkH2OIha-mutDhx4Ow', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     };
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        };
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC__s0jesHKy_J07LkH2OIha-mutDhx4Ow', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     };
     submitHandler = event => {
         event.preventDefault()
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import classes from './Quiz.module.css';
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
+import axios from "../../axios/axios-quiz";
+import Loader from "../../components/UI/Loader/Loader";
 
 class Quiz extends Component {
     state = {
@@ -9,30 +11,8 @@ class Quiz extends Component {
         isFinished: false,
         activeQuestion: 0,
         answerState: null,
-        quiz: [
-            {
-                id: 1,
-                question: 'Какого цвета небо?',
-                rightAnswerId: 2,
-                answers: [
-                    {text: 'Черного', id:1},
-                    {text: 'Синего', id:2},
-                    {text: 'Красного', id:3},
-                    {text: 'Зеленого', id:4}
-                ]
-            },
-            {
-                id: 2,
-                question: 'В каком году основали Санкт-Петербург?',
-                rightAnswerId: 3,
-                answers: [
-                    {text: '1700', id:1},
-                    {text: '1705', id:2},
-                    {text: '1703', id:3},
-                    {text: '1803', id:4}
-                ]
-            }
-        ]
+        quiz: [],
+        loading: true
     };
 
     onAnswerClickHandler = (answerId) => {
@@ -87,6 +67,19 @@ class Quiz extends Component {
             isFinished: false,
             results: {}
         })
+    };
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get(`/quizes/${this.props.match.params.id}.json`);
+            const quiz = response.data;
+            this.setState({
+                quiz,
+                loading: false
+            })
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     render() {
@@ -95,20 +88,19 @@ class Quiz extends Component {
 
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
-                    {
-                        this.state.isFinished
-                        ? <FinishedQuiz
+                    { this.state.loading
+                        ? <Loader/>
+                        : this.state.isFinished
+                            ? <FinishedQuiz
                                 results={this.state.results}
                                 quiz={this.state.quiz}
                                 onRetry={this.retryHandler}
                             />
-                        : <ActiveQuiz
+                            : <ActiveQuiz
                                 answers={this.state.quiz[this.state.activeQuestion].answers} question={this.state.quiz[this.state.activeQuestion].question}
                                 onAnswerClick={this.onAnswerClickHandler} quizLength={this.state.quiz.length}
                                 AnswerNumber={this.state.activeQuestion + 1} answerState={this.state.answerState}
-                                />
-                    }
-
+                            />}
                 </div>
             </div>
         )
